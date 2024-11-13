9b05f1ebf0e265eab980fa1572d3387bb15268e2
Leonardo Santos <leonardodossantoss50@gmail.com>
2024-11-13 02:25:23 -0300

    Adicionado rota para home. Adicionado e estilizado navbar com snippets.

--- fatosedados/blog/views.py
+++ fatosedados/blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.views.decorators.cache import never_cache
+
 from .models import Post, PostImage
+
+def home(request):
+    posts = Post.objects.all().order_by("-number_of_visitors")
+    addtional_images = PostImage.objects.all()
+
+    latest_posts = Post.objects.all()[:3]
+    
+    return render(request, 'blog/post_list.html', {'posts': posts, 'addtional_images': addtional_images, 'latest_posts': latest_posts})
 
 def post_list(request):
     posts = Post.objects.all().order_by("-number_of_visitors")
@@ -9,16 +20,38 @@ def post_list(request):
     
     return render(request, 'blog/post_list.html', {'posts': posts, 'addtional_images': addtional_images, 'latest_posts': latest_posts})
 
+# @never_cache
 def post(request, post_id, title_post):
 
-    post = Post.objects.all().filter(id=post_id).first()
-    post.number_of_visitors += 1
-    post.save()
+    if request.method == "GET":
 
-    postFilter = Post.objects.all().filter(
-        id=post_id
-    ).first()
+        postFilter = Post.objects.all().filter(id=post_id).first()
+        postFilter.number_of_visitors += 1
+        postFilter.save()
+        
+        return render(request, 'blog/post.html', context={'post': postFilter})
+    else:
+        return JsonResponse({"statusCode": 400, "msg": "not found"})
+
+
+# ------------------------------------ REVISAR ESSA ALTERNATIVA ------------------------------------
+# @never_cache
+# def post(request, post_id, title_post):
+#     if request.method == "GET":
+#         # Checa na sessão se o post já foi visualizado
+#         if f"viewed_post_{post_id}" not in request.session:
+#             postFilter = Post.objects.all().filter(id=post_id).first()
+#             postFilter.number_of_visitors += 1
+#             postFilter.save()
+            
+#             # Marca o post como visualizado na sessão
+#             request.session[f"viewed_post_{post_id}"] = True
+
+#         # Pega o post para renderização
+#         postFilter = Post.objects.all().filter(id=post_id).first()
+#         return render(request, 'blog/post.html', context={'post': postFilter})
     
-    return render(request, 'blog/post.html', context={'post': postFilter})
+#     return JsonResponse({"statusCode": 400, "msg": "not found"})
+
 
 
