98eb293c33dd5340e7eb594fa5800abdbe7031f0
Leonardo Santos <leonardodossantoss50@gmail.com>
2024-12-01 12:55:15 -0300

    estilização do menu mobile responsivo.

--- fatosedados/fatosedados/settings.py
+++ fatosedados/fatosedados/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    # 'fatosedados'
+    'fontawesomefree',
 ]
 
 AUTH_USER_MODEL = 'blog.UserRegistration'
